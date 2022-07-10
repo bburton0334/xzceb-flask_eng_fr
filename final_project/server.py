4017fdf9811f07b726d59713fa339385d1332f95
Briana Burton <bburton0334@conestogac.on.ca>
2022-07-10 19:15:49 +0000

    Fixed translation page. Now fully functional

--- final_project/server.py
+++ final_project/server.py
@@ -10,13 +10,15 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Bonjour"
+    result = machinetranslation.translator.english_to_french(textToTranslate)
+    return result
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Hello"
+    result = machinetranslation.translator.french_to_english(textToTranslate)
+    return result
 
 @app.route("/")
 def renderIndexPage():
